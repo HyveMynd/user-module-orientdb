f04bc267bd2d7ad7356e88e374b642dc57d9fe8f
Andres Monroy <andresf.mon@gmail.com>
2014-07-20 16:32:07 -0600

    adding rethinkdb

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 /**
  * Created by HyveMynd on 7/20/14.
  */
+var db = require('secondthought');
+var assert = require('assert');
 
 module.exports = function(grunt){
 
@@ -9,6 +11,17 @@ module.exports = function(grunt){
             files : [ 'server/**/*.js' ]
         }
     });
+
+	grunt.registerTask("installDb", function(){
+		var done = this.async();
+		db.connect({db: "membership"}, function(err, db){
+			db.install(['users', 'logs', 'sessions'], function(err, tableResult){
+				assert.ok(err === null, err);
+				console.log("Db Installed: " + tableResult);
+				done();
+			});
+		});
+	});
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
