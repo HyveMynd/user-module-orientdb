ab41f43140f1128b49c8f1fea04de20661464c56
Andres Monroy <andresf.mon@gmail.com>
2015-02-04 17:35:56 -0700

    more tests for verification

--- test/verification_spec.js
+++ test/verification_spec.js
@@ -14,14 +14,14 @@ describe('Verification', function(){
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth:{
-            user: 'pxtestacct@gmail.com',
+            user: 'pxdevacct@gmail.com',
             pass: 'powpowbang1'
         }
     });
 
     var mailOptions = {
-        from: 'pxtestacct@gmail.com',
-        subject: 'Mail Verification'
+        from: 'pxdevacct@gmail.com',
+        subject: 'Email Verification'
     };
 
     before(function (done) {
@@ -30,13 +30,13 @@ describe('Verification', function(){
         }).done();
     });
 
-    describe("send a verification email", function(){
+    describe("sends a verification email", function(){
         var user = {};
 
         before(function(done){
             userRepo.clear().then(function () {
                 reg.register({
-                    email: "asd@dsa.com",
+                    email: "pxdevacct@gmail.com",
                     password: "a",
                     confirm: "a",
                     firstName: 'asd',
@@ -54,7 +54,7 @@ describe('Verification', function(){
             user.should.be.defined;
         });
         it("saves the verification token", function () {
-            user.verificationToken.should.not.equal(null)
+            user.verificationToken.should.not.equal(null).and.should.not.be.empty;
         });
     });
 
@@ -183,6 +183,114 @@ describe('Verification', function(){
         });
         it("has an error message", function () {
             error.message.should.equal("Must provide a from field and subject")
+        });
+    });
+
+
+    describe("successfully verifies token", function(){
+        var user = {};
+
+        before(function(done){
+            userRepo.clear().then(function () {
+                reg.register({
+                    email: "pxdevacct@gmail.com",
+                    password: "a",
+                    confirm: "a",
+                    firstName: 'asd',
+                    lastName: 'dsa'
+                }).then(function (result) {
+                    reg.sendVerificationEmail(result.email, transporter, mailOptions).then(function (result) {
+                        user = result;
+                        reg.verifyToken(user.email, user.verificationToken).then(function (result) {
+                            user = result;
+                            done();
+                        }).done();
+                    }).done();
+                }).done();
+            });
+        });
+
+        it("should be successful", function () {
+            user.should.be.defined;
+        });
+        it("should have  verified status", function () {
+            user.status.should.equal("Validated");
+        });
+    });
+
+    describe("invalid email", function(){
+        var user = {};
+        var error = null;
+
+        before(function(done){
+            userRepo.clear().then(function () {
+                reg.register({
+                    email: "pxdevacct@gmail.com",
+                    password: "a",
+                    confirm: "a",
+                    firstName: 'asd',
+                    lastName: 'dsa'
+                }).then(function (result) {
+                    reg.sendVerificationEmail(result.email, transporter, mailOptions).then(function (result) {
+                        user = result;
+                        reg.verifyToken(null, user.verificationToken).then(function (result) {
+                            user = result;
+                            done();
+                        }).catch(function (err) {
+                            error = err;
+                            done();
+                        });
+                    }).done();
+                }).done();
+            });
+        });
+
+        it("should fail", function () {
+            error.should.not.equal(null);
+        });
+        it("has a pending status", function () {
+            user.status.should.equal("pending")
+        });
+        it("has an error message", function () {
+            error.message.should.equal("Email is invalid");
+        });
+    });
+
+    describe("invalid token", function(){
+        var user = {};
+        var error = {};
+
+        before(function(done){
+            userRepo.clear().then(function () {
+                reg.register({
+                    email: "pxdevacct@gmail.com",
+                    password: "a",
+                    confirm: "a",
+                    firstName: 'asd',
+                    lastName: 'dsa'
+                }).then(function (result) {
+                    reg.sendVerificationEmail(result.email, transporter, mailOptions).then(function (result) {
+                        user = result;
+                        reg.verifyToken(user.email, "asdasd").then(function (result) {
+                            user = result;
+                            done();
+                        }).catch(function (err) {
+                            error = err;
+                            done();
+                        });
+                    }).done();
+                }).done();
+            });
+        });
+
+        it("should fail", function () {
+            error.should.not.equal(null);
+        });
+        it("has a pending status", function () {
+            user.status.should.equal("pending")
+        });
+        it("has an error message", function () {
+            error.message.should.equal("Token is invalid");
         });
     });
 
